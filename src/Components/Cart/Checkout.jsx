01a652a8ea68e0557139565d957c9cf7ea4ab8d2
zainab3702 <zainabfaiqua03@gmail.com>
2024-01-09 15:01:28 +0530

    Inital commit

---
+++ src/Components/Cart/Checkout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Popup from "./Popup";
+
+const CheckoutApp = () => {
+  return (
+    <div>
+      <h1>React Popup Example</h1>
+      <Popup title="Hello" message="This is a popup" />
+    </div>
+  );
+};
+
+export default CheckoutApp;
